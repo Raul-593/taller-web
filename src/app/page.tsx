f1a07601d28b64aba3fb6b32de4eb01238fcecd4
Raul Viteri <raulviterip@gmail.com>
2026-04-17 17:30:00 -0500

    analytics vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/componentes/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/componentes/ui/cards"
 import Link from "next/link"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-zinc-950 p-4">
       <Card className="w-full max-w-md">
         <CardHeader>
+          <Analytics />
           <CardTitle>Bienvenido a la Página de 593 Cycling Studio</CardTitle>
         </CardHeader>
         <CardContent>
