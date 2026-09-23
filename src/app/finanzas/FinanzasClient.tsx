a975603e3752f7f197260b81cda894a344735535
Raul Viteri <raulviterip@gmail.com>
2026-09-23 16:47:23 -0500

    arreglo props grafico ingreso vs gasto

--- src/app/finanzas/FinanzasClient.tsx
+++ src/app/finanzas/FinanzasClient.tsx
@@ -165,7 +165,7 @@ export function FinanzasClient({
                                     No hay movimientos para el periodo seleccionado.
                                 </div>
                             ) : (
-                                <ResponsiveContainer width="100%" height="100%">
+                                <ResponsiveContainer width="100%" height="100%" minHeight={undefined} minWidth={0}>
                                     <BarChart data={chartData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
                                         <CartesianGrid strokeDasharray="3 3" vertical={false} />
                                         <XAxis dataKey="name" />
